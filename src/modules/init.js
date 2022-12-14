a5b35d03d269e032dafeacc47bd3669be80b5303
Sergey Sudakov <s3gaddtv@gmail.com>
2022-12-14 14:37:13 +0300

    feat: connect operation handlers

--- src/modules/init.js
+++ src/modules/init.js
@@ -1,12 +1,21 @@
 import { exit } from './commands/exit.js';
+import { operationManager } from './operationManager.js';
 
 import { user } from './userInfo.js';
+import { errorHandler } from './errorHandler.js';
 
 import { printMessage } from '../utils/printMessage.js';
 
 export const init = () => {
     printMessage(`Welcome to the File Manager, ${user.name}!`);
     printMessage(`You are currently in ${user.homedir}`)
+
+    process.stdin.on('data', data => {
+        const [ command, ...args ] = data.toString().trim().split(' ');
+        const callback = operationManager[command] || errorHandler.invalidInput;
+
+        callback(args);
+    });
 
     process.on('SIGINT', () => exit());
 };
