15e23ff0266b7dc1934f91075a567c929d21d2c8
Sergey Sudakov <s3gaddtv@gmail.com>
2022-12-15 19:56:52 +0300

    feat: implement cd operation

--- src/modules/commands/operationManager.js
+++ src/modules/commands/operationManager.js
@@ -1,5 +1,6 @@
 import { up } from './up/index.js';
 import { ls } from './ls/index.js';
+import { cd } from './cd/index.js';
 
 import { errorHandler } from '../errorHandler.js';
 
@@ -8,6 +9,11 @@ export const operationManager = {
         if (arg) return errorHandler.invalidInput();
 
         return up();
+    },
+    cd: arg => {
+        if (!arg) return errorHandler.invalidInput();
+
+        return cd(arg);
     },
     ls: arg => {
         if (arg) return errorHandler.invalidInput();
