e38192098fc365816354c866c90486ccc6339a64
Sergey Sudakov <s3gaddtv@gmail.com>
2022-12-19 17:06:11 +0300

    fix: missing dependence

---
+++ src/modules/commands/fs/index.js
@@ -0,0 +1,6 @@
+export { add } from './add/index.js';
+export { cat } from './cat/index.js';
+export { cp } from './cp/index.js';
+export { mv } from './mv/index.js';
+export { rm } from './rm/index.js';
+export { rn } from './rn/index.js';
